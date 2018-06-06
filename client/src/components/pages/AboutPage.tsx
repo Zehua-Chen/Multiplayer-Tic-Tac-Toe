956b2b59617fb53d182889b79f9a026cbf9ee170
Zehua-Chen <peterchen06242000@outlook.com>
2018-06-05 23:25:15 -0500

    Make About Page

--- client/src/components/pages/AboutPage.tsx
+++ client/src/components/pages/AboutPage.tsx
@@ -1,10 +1,50 @@
 import React from 'react';
 
+import "./AboutPage.css";
+
+/**
+ * A page that shows with what dependencies the game is made.
+ */
 class AboutPage extends React.Component {
   render() {
     return (
-      <div className="container-fluid bg-light">
-        <h1>About Page</h1>
+      <div className="container-fluid aboutpage-content-height bg-light">
+        <div className="container">
+        
+          <div className="row">
+            <div className="col mt-3">
+              <h1>About</h1>
+            </div>
+          </div>
+          
+          <div className="row">
+            <div className="col">
+              <h2>Dependencies</h2>
+            </div>
+          </div>
+          
+          <div className="row">
+            <div className="col">
+              <h3>Client Side</h3>
+              <ul>
+                <li><a href="https://reactjs.org/">ReactJS</a></li>
+                <li><a href="https://socket.io/">Socket.io Client</a></li>
+                <li><a href="https://getbootstrap.com/">Bootstrap</a></li>
+                <li><a href="https://www.typescriptlang.org/">Typescript</a></li>
+                <li><a href="https://github.com/axios/axios">AxiosJS</a></li>
+              </ul>
+            </div>
+            
+            <div className="col">
+              <h3>Server Side</h3>
+              <ul>
+                <li><a href="https://expressjs.com/">ExpressJS</a></li>
+                <li><a href="https://socket.io/">Socket.io</a></li>
+                <li><a href="https://www.typescriptlang.org/">Typescript</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
