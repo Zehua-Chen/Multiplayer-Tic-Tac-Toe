439b1dad59cec59bb3178e33d9fbd589deed51e4
Zehua-Chen <peterchen06242000@outlook.com>
2018-06-25 18:13:43 +0800

    Move Code from "CreateGame" to "WelcomePanel"

--- client/src/components/panels/WelcomePanel/WelcomePanel.tsx
+++ client/src/components/panels/WelcomePanel/WelcomePanel.tsx
@@ -1,41 +1,121 @@
 import React from 'react';
-
+import axios from 'axios';
 import { DispatchProp, connect } from 'react-redux';
-import { CreateGame } from './CreateGame';
+
+import {
+  ICreateGameAction,
+  CREATE_SUCCESSFUL
+} from '../../../actions/ICreateGameAction';
 
 type WelcomePanelMode = "create" | "join";
 
-export interface IWelcomePanelProps {
+interface IWelcomePanelProps {
   mode?: WelcomePanelMode;
 }
 
-class WelcomePanel extends React.Component<IWelcomePanelProps & DispatchProp> {
+export interface IWelcomePanelState {
+  playerName: string;
+  invitationCode: string;
+}
+
+class WelcomePanel extends React.Component<IWelcomePanelProps & DispatchProp, IWelcomePanelState> {
+
+  public static defaultProps: IWelcomePanelProps = {
+    mode: "join"
+  }
   
+  constructor(props: IWelcomePanelProps & DispatchProp) {
+    super(props);
+    
+    this.state = {
+      playerName: "",
+      invitationCode: ""
+    }
+  }
+
+  invitationCodeChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ invitationCode: e.target.value });
+  }
+
+  playerNameChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ playerName: e.target.value });
+  }
+
+  joinGame = () => {
+
+  }
+
+  createGame = () => {
+
+    const { playerName, invitationCode } = this.state;
+
+    var createGameRequest: TicTacToe.ICreateGameRequest = {
+      name: playerName,
+      passcode: invitationCode
+    };
+
+    console.log(`Game = ${createGameRequest}`);
+
+    axios.post<TicTacToe.ICreateGameResponse>("/create_game", createGameRequest)
+      .then((response) => {
+        var data = response.data;
+        if (data.success) {
+          //TODO: Dispatch success message
+          console.log("Create Successful");
+          this.props.dispatch<ICreateGameAction>({ type: CREATE_SUCCESSFUL });
+        } else {
+          alert(`Create Game Failed\nReason: ${data.message}`);
+        }
+      });
+  }
+
   public render() {
 
-    // var cardContent = <button className="btn btn-primary btn-block">Create New Game</button>;
-
-    // if (this.props.mode) {
-    //   switch(this.props.mode) {
-    //     case "join":
-    //       cardContent = <button className="btn btn-primary btn-block">Create New Game</button>;
-    //       break;
-    //   }
-    // }
+    // var title = "";
 
     return (
-      <div className="card">
+      <div className="card bg-dark text-white">
         <div className="card-header">
           <h5>Welcome</h5>
         </div>
-        <CreateGame />
+        <div className="card-body">
+          <div className="container">
 
+            <div className="row mt-2">
+              <div className="col">
+                <div className="form-group">
+                  <label>Your Name</label>
+                  <input
+                    className="form-control" type="text"
+                    onChange={this.playerNameChanged} value={this.state.playerName} />
+                </div>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col">
+                <div className="form-group">
+                  <label>Invitation Code</label>
+                  <input
+                    className="form-control" type="text"
+                    onChange={this.invitationCodeChanged} value={this.state.invitationCode} />
+                </div>
+              </div>
+            </div>
+
+            <div className="row mb-2">
+              <div className="col">
+                <button className="btn btn-light btn-block" onClick={this.createGame}>Create!</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-function mapStateToProps(state: {}, ownProps: {}): IWelcomePanelProps {
+function mapStateToProps(state: {}, ownProps: IWelcomePanelProps): {} {
   return {};
 }
 
