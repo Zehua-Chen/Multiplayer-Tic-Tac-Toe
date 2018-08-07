abbff894ce2e456cd65200f18a46d1534e2c1e0e
Zehua-Chen <peterchen06242000@outlook.com>
2018-08-07 13:35:48 +0800

    Add Navbar

--- client/src/components/pages/AboutPage/AboutPage.tsx
+++ client/src/components/pages/AboutPage/AboutPage.tsx
@@ -3,74 +3,52 @@ import { Link } from 'react-router-dom';
 
 import * as styles from './AboutPage.css';
 
+const linkToGithub = "https://github.com/Zehua-Chen/Multiplayer-Tic-Tac-Toe/";
+
 /**
  * A page that shows with what dependencies the game is made.
  */
 class AboutPage extends React.Component {
-  
+
   render() {
-    
+
     return (
-      <div className={`container-fluid ${styles.fullHeight} bg-light`}>
-        <div className="container">
+      <div className={`${styles.fullHeight} bg-light`}>
+        {/* 
+        Has to use navbar-expand to force navitems to be put horizontally
+        no matter the size of the view port
+         */}
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
         
-          <div className="row">
-            <div className="col mt-3">
-              <h1 className="display-4">About</h1>
+          <a className="navbar-brand" href="#!">Multiplayer Tic Tac Toe</a>
+          
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Game</Link>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="#!">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href={linkToGithub}>Github Repository</a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="container">
+
+          <div className="row mt-3">
+            <div className="col text-center">
+              <h1>About</h1>
             </div>
           </div>
-          
           <div className="row">
             <div className="col">
-              <h2>Dependencies</h2>
+              <p>The project is hosted on Github</p>
             </div>
           </div>
-          
-          <div className="row">
-            <div className="col-md-6">
-              <h3>Client Side</h3>
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item bg-light">
-                  <a href="https://reactjs.org/">ReactJS</a>
-                </li>
-                <li className="list-group-item bg-light">
-                  <a href="https://socket.io/">Socket.io Client</a>
-                </li>
-                <li className="list-group-item bg-light">
-                  <a href="https://getbootstrap.com/">Bootstrap</a>
-                </li>
-                <li className="list-group-item bg-light">
-                  <a href="https://www.typescriptlang.org/">Typescript</a>
-                </li>
-                <li className="list-group-item bg-light">
-                  <a href="https://github.com/axios/axios">AxiosJS</a>
-                </li>
-              </ul>
-            </div>
-            
-            <div className="col-md-6">
-              <h3>Server Side</h3>
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item bg-light">
-                  <a href="https://expressjs.com/">ExpressJS</a>
-                </li>
-                <li className="list-group-item bg-light">
-                  <a href="https://socket.io/">Socket.io</a>
-                </li>
-                <li className="list-group-item bg-light">
-                  <a href="https://www.typescriptlang.org/">Typescript</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          
-          <div className="row mt-4">
-            <div className="col">
-              <Link className="btn btn-outline-dark" to="/">Back to the Game</Link>
-            </div>
-          </div>
-          
         </div>
+
       </div>
     );
   }
