89bb4aef645ffe6bf00c3dd9b1398d4ae3efd9e0
Zehua-Chen <peterchen06242000@outlook.com>
2019-05-02 21:08:12 -0500

    [feat] Implement Winner Detection

--- TicTacToe/Board.cs
+++ TicTacToe/Board.cs
@@ -7,6 +7,92 @@ namespace TicTacToe
         public uint Dimension { get; private set; }
         public Cell[] Cells { get; private set; }
         
+        public int Winner
+        {
+            get 
+            {
+                // Find horizontal winner
+                
+                for (uint y = 0; y < this.Dimension; y++)
+                {
+                    int sum = 0;
+                    
+                    for (uint x = 0; x < this.Dimension; x++)
+                    {
+                        sum += this[y, x];
+                    }
+                    
+                    if (sum == this.Dimension * 1)
+                    {
+                        return 1;
+                    }
+                    else if (sum == this.Dimension * -1)
+                    {
+                        return -1;
+                    }
+                }
+                
+                // Find horizontal winner
+                
+                for (uint x = 0; x < this.Dimension; x++)
+                {
+                    int sum = 0;
+                    
+                    for (uint y = 0; y < this.Dimension; y++)
+                    {
+                        sum += this[y, x];
+                    }
+                    
+                    if (sum == this.Dimension * 1)
+                    {
+                        return 1;
+                    }
+                    else if (sum == this.Dimension * -1)
+                    {
+                        return -1;
+                    }
+                }
+                
+                // Find bottom left to top right winner
+                
+                int diagnolSum = 0;
+                
+                for (uint i = 0; i < this.Dimension; i++)
+                {
+                    diagnolSum += this[i, i];
+                }
+                
+                if (diagnolSum == this.Dimension * 1)
+                {
+                    return 1;
+                }
+                else if (diagnolSum == this.Dimension * -1)
+                {
+                    return -1;
+                }
+                
+                // Find top left to bottom right winner
+                
+                diagnolSum = 0;
+                
+                for (uint i = this.Dimension - 1; i >= 0; i--)
+                {
+                    diagnolSum += this[i, i];
+                }
+                
+                if (diagnolSum == this.Dimension * 1)
+                {
+                    return 1;
+                }
+                else if (diagnolSum == this.Dimension * -1)
+                {
+                    return -1;
+                }
+                
+                return 0;
+            }
+        }
+        
         public Board(uint dimension)
         {
             this.Dimension = dimension;
@@ -22,11 +108,6 @@ namespace TicTacToe
         {
             get { return this.Cells[LinearIndex(y, x)].Occupant; }
             set { this.Cells[LinearIndex(y, x)].Occupant = value; }
-        }
-        
-        public void Set(uint y, uint x, int value)
-        {
-            this.Cells[LinearIndex(y, x)].Occupant = value;
         }
         
         private uint LinearIndex(uint y, uint x)
--- TicTacToeTest/BoardTest.cs
+++ TicTacToeTest/BoardTest.cs
@@ -28,5 +28,77 @@ namespace TicTacToeTest
             board[2, 2] = 1;
             Assert.Equal(1, board[2, 2]);
         }
+        
+        [Fact]
+        public void FindWinnerHorizontal()
+        {
+            Board board = new Board(3);
+            
+            // -1 -1 1
+            // 1 1 -1
+            // -1 -1 -1
+            
+            board[0, 0] = -1;
+            board[0, 1] = -1;
+            board[0, 2] = 1;
+            
+            board[1, 0] = 1;
+            board[1, 1] = 1;
+            board[1, 2] = -1;
+            
+            board[2, 0] = -1;
+            board[2, 1] = -1;
+            board[2, 2] = -1;
+            
+            Assert.Equal(-1, board.Winner);
+        }
+        
+        [Fact]
+        public void FindWinnerVertical()
+        {
+            Board board = new Board(3);
+            
+            // -1 -1 1
+            // 1 -1 -1
+            // -1 -1 1
+            
+            board[0, 0] = -1;
+            board[0, 1] = -1;
+            board[0, 2] = 1;
+            
+            board[1, 0] = 1;
+            board[1, 1] = -1;
+            board[1, 2] = -1;
+            
+            board[2, 0] = -1;
+            board[2, 1] = -1;
+            board[2, 2] = 1;
+            
+            Assert.Equal(-1, board.Winner);
+        }
+        
+        [Fact]
+        public void FindWinnerDiagnol()
+        {
+            Board board = new Board(3);
+            
+            // -1 -1 1
+            // 1 -1 -1
+            // -1 1 -1
+            
+            board[0, 0] = -1;
+            board[0, 1] = -1;
+            board[0, 2] = 1;
+            
+            board[1, 0] = 1;
+            board[1, 1] = -1;
+            board[1, 2] = -1;
+            
+            board[2, 0] = -1;
+            board[2, 1] = 1;
+            board[2, 2] = -1;
+            
+            Assert.Equal(-1, board.Winner);
+        }
     }
 }
